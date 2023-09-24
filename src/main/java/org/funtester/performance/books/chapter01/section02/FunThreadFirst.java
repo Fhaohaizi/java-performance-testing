e83afe7fdc2c4f903c85d095366c3829048dc4cb
kaiqiang.fan <kaiqiang.fan@okg.com>
2023-09-24 16:38:10 +0800

    提交章节1代码

--- src/main/java/org/funtester/performance/books/chapter01/section02/FunThreadFirst.java
+++ src/main/java/org/funtester/performance/books/chapter01/section02/FunThreadFirst.java
@@ -1,20 +1,30 @@
 package org.funtester.performance.books.chapter01.section02;
 
+/**
+ * 继承{@link Thread}实现Java多线程
+ */
 public class FunThreadFirst extends Thread {
 
+    /**
+     * 主线程
+     * @param args
+     */
     public static void main(String[] args) {
-        new FunThreadFirst().start();
-        System.out.println("Hello FunTester!    " + Thread.currentThread().getName());
+        new FunThreadFirst().start();// 启动线程
+        System.out.println("Hello FunTester!    " + Thread.currentThread().getName());// 打印线程名称
     }
 
+    /**
+     * 重写{@link Thread#run()}方法
+     */
     @Override
     public void run() {
         try {
-            Thread.sleep(100);
+            Thread.sleep(100);// 睡眠100毫秒
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e);// 抛出运行时异常
         }
-        System.out.println("Hello FunTester!    " + Thread.currentThread().getName());
+        System.out.println("Hello FunTester!    " + Thread.currentThread().getName());// 打印线程名称
     }
 
 }
--- src/main/java/org/funtester/performance/books/chapter01/section02/FunThreadSecond.java
+++ src/main/java/org/funtester/performance/books/chapter01/section02/FunThreadSecond.java
@@ -1,24 +1,31 @@
 package org.funtester.performance.books.chapter01.section02;
 
 /**
- *
+ * 实现{@link Runnable}接口实现Java多线程
  */
 public class FunThreadSecond implements Runnable {
 
+    /**
+     * 主线程
+     * @param args
+     */
     public static void main(String[] args) {
-        FunThreadSecond second = new FunThreadSecond();
-        new Thread(second).start();
-        System.out.println("Hello FunTester!    " + Thread.currentThread().getName());
+        FunThreadSecond second = new FunThreadSecond();// 创建runnable实例
+        new Thread(second).start();// 启动线程
+        System.out.println("Hello FunTester!    " + Thread.currentThread().getName());// 打印线程名称
     }
 
+    /**
+     * 重写{@link Runnable#run()}方法
+     */
     @Override
     public void run() {
         try {
-            Thread.sleep(100);
+            Thread.sleep(100);// 睡眠100毫秒
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e);// 抛出运行时异常
         }
-        System.out.println("Hello FunTester!    " + Thread.currentThread().getName());
+        System.out.println("Hello FunTester!    " + Thread.currentThread().getName());// 打印线程名称
     }
 
 }
